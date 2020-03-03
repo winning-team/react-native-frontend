87e10b2d0df98e5dba7cf7ca1ab26b3509f4657a
Mark King <markpkng@gmail.com>
2020-03-03 18:36:03 -0500

    more things

--- components/game.js
+++ components/game.js
@@ -1,99 +1,142 @@
 import React, { useEffect, useState } from "react";
-import Dpad from "../assets/dpad.svg"
-import { StyleSheet, View, Text, TouchableHighlight, Alert, Dimensions } from "react-native";
-import { VictoryLine, VictoryGroup, VictoryScatter } from "victory-native";
+import Dpad from "../assets/dpad.svg";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight,
+  Dimensions
+} from "react-native";
 import { axiosWithAuth } from "./axiosWithAuth";
-import { background } from "../styles";
-import { render } from "react-dom";
+import { background, brightGreen } from "../styles";
 
-function Move(dir) {
-    console.log(dir)
+export default function Game() {
+  const [roomDescription, setRoomDescription] = useState(null);
+  const [roomTitle, setRoomTitle] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  function Move(dir) {
+    console.log(dir);
     axiosWithAuth().then(axios => {
-      axios.post("api/adv/move/",{
-        direction: dir
-      })
-      .then(function ({data}) {
-        console.log(data)
-      })
-      .catch(function (error){
-        console.log(error)
-      })
-    })
-  };
-
-export default function Game(){
-    return (
+      axios
+        .post("api/adv/move/", {
+          direction: dir
+        })
+        .then(function({ data }) {
+          if (data) {
+            console.log(data);
+            const { title, description, error_msg } = data;
+            if (error_msg) {
+              setErrorMessage(error_msg);
+            } else {
+              setErrorMessage(null);
+            }
+            setRoomTitle(title);
+            setRoomDescription(description);
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    });
+  }
+  return (
     <View style={styles.container}>
-        <View>
-            <Text style={styles.game_response_text}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+      <View style={styles.game_response_text}>
+        <Text style={styles.gameText}>Room name: {roomTitle}</Text>
+        <Text style={styles.gameText}>Room description: {roomDescription}</Text>
+        {errorMessage && (
+          <Text style={styles.gameError}>Error: {errorMessage}</Text>
+        )}
+      </View>
+      <View style={styles.dpad}>
+        <Dpad width={250} height={250} />
+      </View>
+      <View style={styles.dpad_overlay}>
+        {/* up */}
+        <TouchableHighlight
+          onPress={() => Move("n")}
+          style={styles.dpad_button}
+          underlayColor='rgba(0,0,0,0.2)'
+        >
+          <View />
+        </TouchableHighlight>
+        <View style={styles.mid_row}>
+          {/* left */}
+          <TouchableHighlight
+            onPress={() => Move("w")}
+            style={styles.dpad_button}
+            underlayColor='rgba(0,0,0,0.2)'
+          >
+            <View />
+          </TouchableHighlight>
+          {/* spacing */}
+          <View style={styles.dpad_button} />
+          {/* right */}
+          <TouchableHighlight
+            onPress={() => Move("e")}
+            style={styles.dpad_button}
+            underlayColor='rgba(0,0,0,0.2)'
+          >
+            <View />
+          </TouchableHighlight>
         </View>
-        <View style={styles.dpad}>
-          <Dpad width={250} height={250}/>
-        </View>
-        <View style={styles.dpad_overlay}>
-          {/* up */}
-          <TouchableHighlight onPress={() => Move("n")} style={styles.dpad_button} underlayColor='rgba(0,0,0,0.2)'>
-            <View/>
-          </TouchableHighlight>
-          <View style={styles.mid_row}>
-            {/* left */}
-            <TouchableHighlight onPress={() => Move("w")} style={styles.dpad_button} underlayColor='rgba(0,0,0,0.2)'>
-            <View/>
-          </TouchableHighlight>
-            {/* spacing */}
-            <View style={styles.dpad_button}/>
-            {/* right */}
-            <TouchableHighlight onPress={() => Move("e")} style={styles.dpad_button} underlayColor='rgba(0,0,0,0.2)'>
-            <View/>
-          </TouchableHighlight>
-          </View>
-          {/* down */}
-          <TouchableHighlight onPress={() => Move("s")} style={styles.dpad_button} underlayColor='rgba(0,0,0,0.2)'>
-            <View/>
-          </TouchableHighlight>
-        </View>
-       </View>
-    );
+        {/* down */}
+        <TouchableHighlight
+          onPress={() => Move("s")}
+          style={styles.dpad_button}
+          underlayColor='rgba(0,0,0,0.2)'
+        >
+          <View />
+        </TouchableHighlight>
+      </View>
+    </View>
+  );
 }
 
-const width = Dimensions.get('window').width;
+const width = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "flex-start",
-      backgroundColor: background
-    },
-    game_response_text: {
-        width: width - 32,
-        padding: 8,
-        marginTop: 32,
-        borderBottomColor: 'black',
-        textAlignVertical: 'top',
-        borderWidth: 3,
-        borderRadius: 10,
-        borderStyle: 'solid',
-        color: 'red'
-    },
-    dpad: {
-        position: "absolute",
-        bottom: -37,
-        left: 0,
-      },
-      dpad_overlay:{
-        position: "absolute",
-        left: 39,
-        bottom: 31,
-        justifyContent: "center",
-        alignItems: "center",
-        marginRight: 3.1
-      },
-      dpad_button: {
-        height: 56,
-        width: 56,
-      },
-      mid_row: {
-        flexDirection: 'row',
-      },
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    backgroundColor: background
+  },
+  game_response_text: {
+    width: width - 32,
+    padding: 8,
+    marginTop: 32,
+    borderBottomColor: "black",
+    textAlignVertical: "top",
+    borderWidth: 3,
+    borderRadius: 10,
+    borderStyle: "solid"
+  },
+  dpad: {
+    position: "absolute",
+    bottom: -37,
+    left: 0
+  },
+  dpad_overlay: {
+    position: "absolute",
+    left: 39,
+    bottom: 31,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 3.1
+  },
+  gameText: {
+    color: brightGreen
+  },
+  gameError: {
+    color: "red"
+  },
+  dpad_button: {
+    height: 56,
+    width: 56
+  },
+  mid_row: {
+    flexDirection: "row"
+  }
+});
