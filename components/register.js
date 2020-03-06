06bec312fb6ab50e52e2fe41847fcc6d4360a8be
Mark King <markpkng@gmail.com>
2020-03-06 02:48:32 -0500

    bannaanananan peanut butters

--- components/register.js
+++ components/register.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Text, TouchableHighlight } from "react-native";
 import styled from "styled-components";
 import axios from "axios";
